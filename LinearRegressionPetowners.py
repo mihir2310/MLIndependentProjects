cca91e41f07b1832a03a10db10601228062f2675
Mihir <67392640+mihir2310@users.noreply.github.com>
2021-05-15 23:14:22 -0400

    Create LinearRegressionPetowners.py
    This ML algorithm predicts the probability a dog owner would also own a cat based on differing values of dog ownership in different states across the United States.
    
    Data compiled from World Population Report @ https://worldpopulationreview.com/en/state-rankings/pet-ownership-statistics-by-state

---
+++ LinearRegressionPetowners.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import matplotlib.pyplot as plt
+
+col_list = ['State','percAllPets','percDogOwners','percCatOwners']
+csv = pd.read_csv('C:\\Users\\mihir\\Downloads\\csvData.csv', usecols=col_list)
+doglist = []
+catlist = []
+doglist2 = []
+catlist2 = []
+for i in range(len(csv['percDogOwners'])):
+    doglist.append(csv.iloc[i]['percDogOwners'])
+    catlist.append(csv.iloc[i]['percCatOwners'])
+for i in range(len(doglist)):
+    doglist2.append(int(doglist[i]*100))
+for i in range(len(catlist)):
+    catlist2.append(int(catlist[i]*100))
+
+doglist2 = np.array(doglist2)
+doglist2 = np.reshape(doglist2, (50, 1))
+catlist2 = np.array(catlist2)
+
+#Classifier
+clf = LinearRegression()
+clf.fit(doglist2, catlist2)
+print(clf.predict([[0]]))
+
+#Print Graph
+plt.scatter(catlist2, doglist2)
+plt.show()
